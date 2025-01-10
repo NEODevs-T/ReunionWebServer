4af99ae1377b32fb33fdf450687435e940ac04ec
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2025-01-10 13:13:31 -0400

    Cambios para solucionar el valos por default en la suplencia

--- ReunionDiaApi/Models/DbNeoContext.cs
+++ ReunionDiaApi/Models/DbNeoContext.cs
@@ -125,11 +125,7 @@ namespace ReunionDiaApi.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=10.20.1.60\\DESARROLLO;Initial Catalog=DbNeo;TrustServerCertificate=True;Persist Security Info=True;User ID=UsrEncNeo;Password=L3C7U3A2023*");
-            }
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
