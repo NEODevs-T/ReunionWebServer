079af65b8f14cde75c189df2c6e9a91829e6f280
Humberto7913 <humbertoalfonso8@gmail.com>
2025-09-17 11:53:50 -0400

    Se modifico KsfDTO para mostrar el campo en ingles

--- ReunionWeb/DTOs/ReunionDiaria/KsfDTO.cs
+++ ReunionWeb/DTOs/ReunionDiaria/KsfDTO.cs
@@ -7,8 +7,8 @@ namespace ReunionWeb.ReunionDiaria.DTOs;
 public partial class KsfDTO
 {
     public int Idksf { get; set; }
-
     public string KsfNombre { get; set; } = null!;
-
     public bool KsfEsta { get; set; }
+    public string KsfEnglish { get; set; } = null!;
 }
+
